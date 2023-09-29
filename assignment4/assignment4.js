51e46f6bb681d1e8077b750181c42719aa498211
Mrudula <mrudula.mm@coddletech.com>
2023-09-29 18:10:03 +0530

    commit

--- assignment4/assignment4.js
+++ assignment4/assignment4.js
@@ -5,10 +5,10 @@ $(document).ready(function () {
     
        var checker = 0;
         for (var i = 0; i < 8; i++) {
-            var card = $("div").filter(".box")[i];//main div of single item
-            var title = $("p").filter(".cartname")[i].innerText.toUpperCase();//name
+            var card = $("div").filter(".box")[i];
+            var title = $("p").filter(".cartname")[i].innerText.toUpperCase();
     
-            if (title.indexOf($("#inp").val().toUpperCase()) > -1) {//searchbox id
+            if (title.indexOf($("#inp").val().toUpperCase()) > -1) {
                 card.style.display = 'block';
                 checker++;
             }
@@ -18,7 +18,7 @@ $(document).ready(function () {
         }
         if(checker > 0){
     
-            $("#not_find_any_thing").text("");//search display message
+            $("#not_find_any_thing").text("");
         }
         else{
             $("#not_find_any_thing").text("Product not available").fadeIn();
@@ -32,7 +32,7 @@ $(document).ready(function () {
         var clickedBox = $(this).closest('.box');
         var img_src = clickedBox.find('.images').attr('src');
         var titlte = clickedBox.find('.cartname').text();
-        var price = parseInt(clickedBox.find('.bottom h2').text().replace('$', '')); // Parse the price as a float
+        var price = parseFloat(clickedBox.find('.bottom h2').text().replace('$', ''));
 
             var cart_item = `
                 <div class="cart_item">
@@ -47,7 +47,7 @@ $(document).ready(function () {
                     </div>
 
                     <div class="col" id="t_price">
-                        <h6 class="qtyNumber"><span class="quantity">1</span> X <span class="cart_item_price">$${price}</span></h6>
+                        <h6 class="qtyNumber"><span class="quantity">1</span> X <span class="cart_item_price">$${price.toFixed(2)}</span></h6>
                     </div>
                     <div>
                     <button class="decrement">-</button>
@@ -55,16 +55,31 @@ $(document).ready(function () {
                 </div>
             `;
             $("#order").append(cart_item);
+            updateTotalAmount();
         
         $("#cart_item_count").text(cart_item_count);
         item_count();
-    });
+    }); 
+    
+    function updateTotalAmount(){
+        var totalAmount = 0;
+        $('.cart_item').each(function (){
+            var quantity = parseInt($(this).find('.quantity').text());
+            var price = parseFloat($(this).find('.cart_item_price').text().replace('$', ''));
+            var subtotal = quantity * price;
+            totalAmount += subtotal;
+
+        });
+        $('#total_amount').text('$' + totalAmount);
+    }
+    updateTotalAmount();
     
     $('#order').on('click', '.increment', function() {
         var cartItem = $(this).closest('.cart_item');
         var currentQuantity = parseInt(cartItem.find('.quantity').text());
         currentQuantity++;
         cartItem.find('.quantity').text(currentQuantity);
+        updateTotalAmount();
     });
     
     $('#order').on('click', '.decrement', function() {
@@ -78,6 +93,7 @@ $(document).ready(function () {
             cartItem.remove();
             $("#cart_item_count").text(cart_item_count);
         }
+        updateTotalAmount();
     });
     function item_count(){
         var res = $("#order").children().length;
